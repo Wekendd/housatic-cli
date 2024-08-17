2bfd960c3bb6389afae924c810bba6c0ca670d63
Wekend <96594839+Wekendd@users.noreply.github.com>
2024-08-17 16:34:09 -0700

    basic interpreter layout

---
+++ src/interpreter/lexer.js
@@ -0,0 +1,10 @@
+class Lexer {
+	constructor(input) {
+		this.input = input;
+		this.position = 0;
+	}
+
+	nextToken() {
+		// Logic to produce the next token from the input string
+	}
+}
---
+++ src/interpreter/parser.js
@@ -0,0 +1,10 @@
+class Parser {
+	constructor(tokens) {
+		this.tokens = tokens;
+		this.currentTokenIndex = 0;
+	}
+
+	parse() {
+		// Recursive descent or another parsing method to build the AST
+	}
+}
