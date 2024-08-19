7780f74e90562f245c4ab04616283f1700197c2a
Wekend <96594839+Wekendd@users.noreply.github.com>
2024-08-18 21:02:53 -0700

    removed unnecessary os import

--- src/path.js
+++ src/path.js
@@ -1,12 +1,11 @@
 const fs = require("fs");
-const os = require("os");
 const path = require("path");
 
 let platformPath;
 if (process.platform == "win32") {
     platformPath = path.join(process.env.APPDATA, "housatic"); // %APPDATA%\housatic (Windows)
 } else if (process.platform == "linux") {
-    platformPath = path.join(os.homedir(), "housatic"); // ~/housatic (Linux)
+    platformPath = path.join(process.env.HOME, "housatic"); // ~/housatic (Linux)
 } else if (process.platform == "darwin") {
     platformPath = path.join(process.env.HOME, "Library", "Application Support", "housatic"); // ~/Library/Application Support/housatic (MacOS)
 } else {
